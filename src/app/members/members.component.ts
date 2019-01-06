ed161a937fb38becb7bf37fbcb3c55aa35e7d6bc
Ken Weaver <kbweaver221@gmail.com>
2019-01-06 14:54:34 -0500

    Actual initial commit

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../member';
+import { MEMBERS } from '../mock-members';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css']
+})
+export class MembersComponent implements OnInit {
+
+  members = MEMBERS;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/mock-members.ts
@@ -0,0 +1,6 @@
+import { Member } from './member';
+
+export const MEMBERS: Member[] = [
+  {id: 10, name: 'Ken Weaver'},
+  {id: 11, name: 'Mitch Chanin'}
+]
