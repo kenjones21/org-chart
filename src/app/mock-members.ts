a06d2135b084279ed12e179b3cb848ec670c08c0
Ken Weaver <kbweaver221@gmail.com>
2019-03-25 21:23:53 -0400

    filled in static chart

--- src/app/mock-members.ts
+++ src/app/mock-members.ts
@@ -1,6 +1,6 @@
 import { Member } from './member';
 
 export const MEMBERS: Member[] = [
-  {id: 10, name: 'Ken Weaver'},
-  {id: 11, name: 'Mitch Chanin'}
+  {id: 10, name: 'Ken Weaver', skills: []},
+  {id: 11, name: 'Mitch Chanin', skills: []}
 ]
