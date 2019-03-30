14d11b59bcd537c19786860a39930b9ff08160a9
Ken Weaver <kbweaver221@gmail.com>
2019-03-29 20:27:05 -0400

    fill chart from data

--- src/app/mock-skills.ts
+++ src/app/mock-skills.ts
@@ -2,5 +2,17 @@ import { Skill } from './skill';
 
 export const SKILLS: Skill[] = [
   {id: 1, name: 'Canvassing', category: 'public'},
-  {id: 2, name: 'Tabling', category: 'public'}
+  {id: 2, name: 'Tabling', category: 'public'},
+  {id: 3, name: 'Public Speaking', category: 'public'},
+  {id: 4, name: 'Writing', category: 'writing'},
+  {id: 5, name: 'Editing', category: 'writing'},
+  {id: 6, name: 'Liason', category: 'liason'},
+  {id: 7, name: 'Political Research', category: 'research'},
+  {id: 8, name: 'Technical Research', category: 'research'},
+  {id: 9, name: 'Legal Research', category: 'research'},
+  {id: 10, name: 'Action Organizing', category: 'action'},
+  {id: 11, name: 'Action Lead', category: 'action'},
+  {id: 12, name: 'Action Participation', category: 'action'},
+  {id: 13, name: 'Action Network', category: 'organizing'},
+  {id: 14, name: 'Organizing Campaign Info', category: 'organizing'}
 ]
